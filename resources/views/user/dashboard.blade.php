6c2f68463ba00887d35c3380d19fe532403dbd3e
unknown <skabirgithub>
2025-02-25 13:15:51 +0600

    Code Update at 25 February,2025  1:15:50 pm Tuesday for -

--- resources/views/user/dashboard.blade.php
+++ resources/views/user/dashboard.blade.php
@@ -9,22 +9,25 @@ senten.'])
 
 <div class="container">
     <div class="row">
-        @if(is_null(auth()->user()->validity) || auth()->user()->validity->isPast())
-            <div class="col-md-12">
-                <div class="alert alert-warning">
-                    Your account validity has expired or new. Please make a payment of BDT 10 to renew your account.
-                </div>
-                <form action="{{ route('user.payment') }}" method="POST">
-                    @csrf
-                    <div class="form-group">
-                        <input type="hidden" name="amount" id="amount" value="10" class="form-control" required>
-                        <input type="hidden" name="type" id="type" value="membership" class="form-control" required>
-                        <input type="hidden" name="type_id" id="type" value="{{Auth::user()->id}}" class="form-control" required>
-                    </div>
-                    <button type="submit" class="btn btn-brand">Pay Now</button>
-                </form>
+        @if(is_null(auth()->user()->profile->validity) ||
+        \Carbon\Carbon::parse(auth()->user()->profile->validity)->isPast())
+        <div class="col-md-12">
+            <div class="alert alert-warning">
+                Your account validity has expired or is new. Please make a payment of BDT 10 to renew your account.
             </div>
+            <form action="{{ route('user.payment') }}" method="POST">
+                @csrf
+                <div class="form-group">
+                    <input type="hidden" name="amount" id="amount" value="10" class="form-control" required>
+                    <input type="hidden" name="type" id="type" value="membership" class="form-control" required>
+                    <input type="hidden" name="type_id" id="type_id" value="{{ Auth::user()->id }}" class="form-control"
+                        required>
+                </div>
+                <button type="submit" class="btn btn-brand">Pay Now</button>
+            </form>
+        </div>
         @endif
+
     </div>
 </div>
 
@@ -33,29 +36,29 @@ senten.'])
     <div class="row">
         <div class="col-md-4">
             <a href="{{route('user.jobs.index')}}">
-            <div class="card-counter primary">
-                <i class="fa fa-code-fork"></i>
-                <span class="count-numbers">{{$jobs}}</span>
-                <span class="count-name">Jobs</span>
-            </div>
-        </a>
+                <div class="card-counter primary">
+                    <i class="fa fa-code-fork"></i>
+                    <span class="count-numbers">{{$jobs}}</span>
+                    <span class="count-name">Jobs</span>
+                </div>
+            </a>
         </div>
 
         <div class="col-md-4"><a href="{{route('user.blogs.index')}}">
-            <div class="card-counter danger">
-                <i class="fa fa-ticket"></i>
-                <span class="count-numbers">{{$blogs}}</span>
-                <span class="count-name">Blogs</span>
-            </div>
+                <div class="card-counter danger">
+                    <i class="fa fa-ticket"></i>
+                    <span class="count-numbers">{{$blogs}}</span>
+                    <span class="count-name">Blogs</span>
+                </div>
             </a>
         </div>
 
         <div class="col-md-4"><a href="{{route('user.zooms.index')}}">
-            <div class="card-counter success">
-                <i class="fa fa-database"></i>
-                <span class="count-numbers">{{$zooms}}</span>
-                <span class="count-name">Meeting</span>
-            </div>
+                <div class="card-counter success">
+                    <i class="fa fa-database"></i>
+                    <span class="count-numbers">{{$zooms}}</span>
+                    <span class="count-name">Meeting</span>
+                </div>
             </a>
         </div>
 
